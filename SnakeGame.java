5e05321fda57b17007925b1cb49207ea2ad67a5e
Yash C Jaware <72185189+yashj0511@users.noreply.github.com>
2025-07-18 19:01:19 +0530

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -2,7 +2,35 @@ package snakegame;
 
 import javax.swing.JFrame;
 
+public class SnakeGame extends JFramepackage snakegame;
+
+import javax.swing.JFrame;
+
 public class SnakeGame extends JFrame
+{
+
+	SnakeGame()
+	{
+		//setLocation(700,400);
+		super("Snake Game");
+		add(new Board());
+		pack();		//refresh the frame
+		
+		
+		setSize(300,300);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		
+	}
+	public static void main(String[] args) 
+	{
+		
+		new SnakeGame();
+		
+	}
+}
+
 {
 
 	SnakeGame()
